5ad98f7926b0078330cc6cee921177b234d0110f
hagar2003 <hagaradeahmed386@gmail.com>
2024-07-27 21:21:05 +0300

    Notes view appbar

---
+++ lib/widgets/notes_view_body.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class NotesViewBody extends StatelessWidget {
+  const NotesViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData.dark(),
+        debugShowCheckedModeBanner: false,
+        home:  Scaffold(
+          // appBar: AppBar(
+          //   title: const Text('Notes',style: TextStyle(fontSize: 28,),),
+          //   actions: const [
+          //     Icon(
+          //       Icons.search,
+          //       size: 30,
+          //     )
+          //   ],
+          // ),
+          body: Column(children: [const SizedBox(height: 45,),
+            Padding(
+              padding: const EdgeInsets.only(left: 16.0,right: 16),
+              child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [const Text('Notes',style: TextStyle(fontSize: 28,),),
+              Container(width: 40,height: 40,
+               decoration:  BoxDecoration(color: Colors.white.withOpacity(.1),
+               borderRadius :BorderRadius.circular(16)),
+               child:const Icon(Icons.search,size: 30,)) ],),
+               
+            )
+          ],),
+
+        ));
+  }
+}
