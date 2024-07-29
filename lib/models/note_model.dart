6aeb447c4007f9119ed16d6bfd8ae44f5324bcdf
hagar2003 <hagaradeahmed386@gmail.com>
2024-07-29 17:29:39 +0300

    create type adapter for note model

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,7 +1,15 @@
-class NoteModel {
+import 'package:hive/hive.dart';
+//import 'package:build_runner/build_runner.dart';
+part 'note_model.g.dart';
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject{
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String subtitle;
+  @HiveField(2)
   final String date;
+  @HiveField(3)
   final int color;
   NoteModel(
       {required this.title,
--- lib/widgets/note_item.dart
+++ lib/widgets/note_item.dart
@@ -16,7 +16,7 @@ class NoteItem extends StatelessWidget {
         width: 400,
         padding: const EdgeInsets.only(top: 12, bottom: 12, right: 8),
         decoration: BoxDecoration(
-          color: Colors.yellow,
+          color: const Color.fromARGB(255, 213, 201, 240),
           borderRadius: BorderRadius.circular(16),
         ),
         child: Column(
