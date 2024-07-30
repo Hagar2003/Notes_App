55b86540afa8e2daa352bc27a0873c0c83d7cbfc
hagar2003 <hagaradeahmed386@gmail.com>
2024-07-30 19:17:46 +0300

    provide add note cubit

--- lib/cubits/cubit/note_cubit_dart_cubit.dart
+++ lib/cubits/cubit/note_cubit_dart_cubit.dart
@@ -1,13 +1,19 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
-
+import 'package:hive/hive.dart';
+import 'package:notes_app/models/note_model.dart';
 
 part 'add_note_cubit.dart';
 
 class NoteCubitDartCubit extends Cubit<NoteCubitDartState> {
   NoteCubitDartCubit() : super(NoteCubitDartInitial());
-  addNote(NoteModel){
-
+  addNote(NoteModel note) async {
+    emit(NoteSuccess());
+    try {
+      var notesBx = Hive.box<NoteModel>('notesBox');
+      await notesBx.add(note);
+    } on Exception catch (e) {
+      NoteFailure( errorMessage: e.toString());
+    }
   }
-  
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:notes_app/cubits/cubit/note_cubit_dart_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/notes_view.dart';
+
 
 //import 'package:build_runner/build_runner.dart';
 void main() async {
   await Hive.initFlutter();
   await Hive.openBox('notes_box');
   Hive.registerAdapter(NoteModelAdapter());
-  runApp(const NotesApp());
+  runApp( const NotesApp());
 }
 
 class NotesApp extends StatelessWidget {
@@ -16,11 +19,17 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-            brightness: Brightness.dark,
-            fontFamily: 'assets/Poppins-Regular.ttf'),
-        home: const NotesView());
+    return 
+     MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => NoteCubitDartCubit())
+    
+      
+      ],
+      child: Container(),
+     )
+    ;
+    
   }
 }
